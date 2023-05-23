82306df8983f40aa1d39922e215a89a4bbc39b66
Daryn Tussupbekov <daryn.tussupbekov@dnapayments.com>
2023-05-23 21:59:25 +0600

    first commit

---
+++ src/Table.jsx
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react'
+
+const Table = () => {
+    const [tableParams, setTableParams] = useState({});
+
+    useEffect(() => {
+        getTableParams()
+    },[])
+
+    const getTableParams = () => {
+        const queryParameters = new URLSearchParams(window.location.search)
+        const number = queryParameters.get("number")
+        const place = queryParameters.get("place")
+
+        setTableParams({number, place})
+    }
+    
+
+    return <div>
+        <h1>Table</h1>
+        <div>Table Number: {tableParams.number}</div>
+        <div>ID: {tableParams.place}</div>
+    </div>
+}
+
+export default Table
